35c75dc06067d26e0aded08ed7d51be54acdb141
Yuya Hirayama <yuya-hirayama@cookpad.com>
2018-10-04 20:11:30 +0900

    Initial Commit

---
+++ YosageSchema/YosageSchema.h
@@ -0,0 +1,19 @@
+//
+//  YosageSchema.h
+//  YosageSchema
+//
+//  Created by yuya-hirayama on 2018/10/04.
+//  Copyright © 2018年 hiragram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YosageSchema.
+FOUNDATION_EXPORT double YosageSchemaVersionNumber;
+
+//! Project version string for YosageSchema.
+FOUNDATION_EXPORT const unsigned char YosageSchemaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YosageSchema/PublicHeader.h>
+
+
